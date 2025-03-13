e0066f004b0e96b44a61af166337787390112a19
theredwiking <hjulmandn@gmail.com>
2025-03-13 23:45:28 +0100

    Need to learn tokenizer, currently does not work

--- src/gemini/tokenizer.zig
+++ src/gemini/tokenizer.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+const mem = std.mem;
+
 pub const TokenType = enum {
     ILLEGAL,
     EOF,
@@ -10,17 +12,25 @@ pub const TokenType = enum {
     QuoteLine,
 };
 
-pub const Token = struct { type: TokenType, literal: ?[]u8, line: []const u8 };
+pub const Token = struct { type: TokenType, line: []const u8 };
 
-pub fn loopText(allocator: std.mem.Allocator, input: std.ArrayList([]const u8)) !std.ArrayList(Token) {
+// This is not the final tokenizer, this is an prototype. I need to learn more about how they work.
+// Also need to learn how to do this most optimal
+pub fn loopText(allocator: mem.Allocator, input: std.ArrayList([]const u8)) !std.ArrayList(Token) {
     for (input.items) |chunck| {
-        var splitted = std.mem.splitAny(u8, chunck, "\n");
+        var splitted = mem.splitAny(u8, chunck, "\n");
 
-        while (splitted.next()) |line| {
-            if (line.len != 0) {
-                std.debug.print("'{s}'\n", .{line});
-            }
-        }
+        //while (splitted.next()) |line| {
+        //    if (line.len != 0) {
+        //        std.debug.print("'{s}'\n", .{line});
+        //        const result = switch (line[0]) {
+        //            '#' => Token{ .type = .HeadingLine, .line = line },
+        //            '=' => Token{ .type = .LinkLine, .line = line },
+        //            else => Token{ .type = .TextLine, .line = line },
+        //        };
+        //        std.debug.print("{}", .{result.type});
+        //    }
+        //}
     }
     return std.ArrayList(Token).init(allocator);
 }
