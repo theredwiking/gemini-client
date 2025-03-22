3635e8330354cbfa35d49ef11d6a6c2c359831a6
theredwiking <hjulmandn@gmail.com>
2025-03-22 17:28:45 +0100

    Working prototype tokenizer

--- src/gemini/tokenizer.zig
+++ src/gemini/tokenizer.zig
@@ -5,6 +5,7 @@ const mem = std.mem;
 pub const TokenType = enum {
     ILLEGAL,
     EOF,
+    StatusLine,
     TextLine,
     LinkLine,
     HeadingLine,
@@ -15,22 +16,42 @@ pub const TokenType = enum {
 pub const Token = struct { type: TokenType, line: []const u8 };
 
 // This is not the final tokenizer, this is an prototype. I need to learn more about how they work.
-// Also need to learn how to do this most optimal
+// Probably needs to be optimized
 pub fn loopText(allocator: mem.Allocator, input: std.ArrayList([]const u8)) !std.ArrayList(Token) {
+    var tokens = std.ArrayList(Token).init(allocator);
     for (input.items) |chunck| {
         var splitted = mem.splitAny(u8, chunck, "\n");
 
-        //while (splitted.next()) |line| {
-        //    if (line.len != 0) {
-        //        std.debug.print("'{s}'\n", .{line});
-        //        const result = switch (line[0]) {
-        //            '#' => Token{ .type = .HeadingLine, .line = line },
-        //            '=' => Token{ .type = .LinkLine, .line = line },
-        //            else => Token{ .type = .TextLine, .line = line },
-        //        };
-        //        std.debug.print("{}", .{result.type});
-        //    }
-        //}
+        while (splitted.next()) |line| {
+            if (line.len != 0) {
+                switch (line[0]) {
+                    '#' => {
+                        try tokens.append(.{ .type = .HeadingLine, .line = line });
+                    },
+                    '=' => {
+                        if (line[1] == '>') {
+                            try tokens.append(.{ .type = .LinkLine, .line = line });
+                        }
+                    },
+                    '*' => {
+                        try tokens.append(.{ .type = .ListItem, .line = line });
+                    },
+                    '>' => {
+                        try tokens.append(.{ .type = .QuoteLine, .line = line });
+                    },
+                    // TODO: Check status and take action if needed
+                    '0'...'6' => {
+                        try tokens.append(.{ .type = .StatusLine, .line = line });
+                    },
+                    else => {
+                        try tokens.append(.{ .type = .TextLine, .line = line });
+                    },
+                }
+            }
+        }
     }
-    return std.ArrayList(Token).init(allocator);
+    for (tokens.items) |token| {
+        std.debug.print("Type: {}, Line: {s}\n", .{ token.type, token.line });
+    }
+    return tokens;
 }
